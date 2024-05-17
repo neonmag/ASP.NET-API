facead6a8e1f04bcb79a251cc9f89b8afd78d595
Administrator <admin@example.com>
2024-05-17 14:45:31 +0000

    Update Screenshot.cs

--- Entity/Profile/Screenshot.cs
+++ Entity/Profile/Screenshot.cs
@@ -1,36 +1,4 @@
-<<<<<<< HEAD
-﻿using Slush.Entity.Abstract;
 
-namespace Slush.Data.Entity.Profile
-{
-    public class Screenshot : Post
-    {
-        public String screenshotUrl { get; set; }
-
-        public Screenshot(String id,
-                         String authorId,
-                         String gameId,
-                         String discussionId,
-                         int dislikesCount,
-                         int likesCount,
-                         String? description,
-                         String screenshotUrl,
-                         String title,
-                         DateTime createdAt)
-        {
-            this.id = id;
-            this.likesCount = likesCount;
-            this.gameId = gameId;
-            this.authorId = authorId;
-            this.screenshotUrl = screenshotUrl;
-            this.discussionId = discussionId;
-            this.dislikesCount = dislikesCount;
-            this.description = description;
-            this.title = title;
-            this.createdAt = createdAt;
-        }
-=======
-﻿
 
 namespace Slush.Data.Entity.Profile
 {
@@ -65,6 +33,5 @@ namespace Slush.Data.Entity.Profile
         public String? screenshotUrl { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
