d3d6761274c63f43aa032356ff5589144fe8bcda
Administrator <admin@example.com>
2024-05-17 14:42:31 +0000

    Update Friends.cs

--- Entity/Profile/Friends.cs
+++ Entity/Profile/Friends.cs
@@ -1,29 +1,16 @@
-﻿namespace Slush.Entity.Profile
+namespace Slush.Entity.Profile
 {
     public class Friends
     {
-<<<<<<< HEAD
-        public String id { get; set; }
-        public String userId { get; set; }
-        public String friendId { get; set; }
-
-        public Friends(String id,
-                       String userId,
-                       String friendId)
-=======
         public Friends()
         {
         }
 
         public Friends(Guid id, Guid userId, Guid friendId, DateTime? createdAt)
->>>>>>> development_branch
         {
             this.id = id;
             this.userId = userId;
             this.friendId = friendId;
-<<<<<<< HEAD
-        }
-=======
             this.createdAt = createdAt;
         }
 
@@ -33,6 +20,5 @@
 
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
