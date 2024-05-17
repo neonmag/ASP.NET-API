2dfd7f376f812b3a19fc893babc35937e2175bd2
Administrator <admin@example.com>
2024-05-17 14:48:33 +0000

    Update Video.cs

--- Entity/Profile/Video.cs
+++ Entity/Profile/Video.cs
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-﻿using Slush.Entity.Abstract;
-
-namespace Slush.Data.Entity.Profile
-{
-    public class Video : Post
-    {
-        public String videoUrl { get; set; }
-
-        public Video(String id,
-                         String authorId,
-                         String gameId,
-                         String discussionId,
-                         int dislikesCount,
-                         int likesCount,
-                         String? description,
-                         String videoUrl,
-                         String title,
-                         DateTime createdAt)
-        {
-            this.id = id;
-            this.likesCount = likesCount;
-            this.gameId = gameId;
-            this.authorId = authorId;
-            this.videoUrl = videoUrl;
-            this.discussionId = discussionId;
-            this.dislikesCount = dislikesCount;
-            this.description = description;
-            this.title = title;
-            this.createdAt = createdAt;
-        }
-=======
-﻿
 
 namespace Slush.Data.Entity.Profile
 {
@@ -66,6 +33,5 @@ namespace Slush.Data.Entity.Profile
         public String? videoUrl { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
