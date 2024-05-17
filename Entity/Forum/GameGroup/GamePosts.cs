733f2b042eea4677338fcf280126dd57c25b2d8d
Administrator <admin@example.com>
2024-05-17 14:33:39 +0000

    Update GamePosts.cs

--- Entity/Forum/GameGroup/GamePosts.cs
+++ Entity/Forum/GameGroup/GamePosts.cs
@@ -1,38 +1,7 @@
-﻿using Slush.Data.Entity.Profile;
+using Slush.Data.Entity.Profile;
 
 namespace Slush.Data.Entity.Community.GameGroup
 {
-<<<<<<< HEAD
-    public class GamePosts : Slush.Entity.Abstract.Post
-    {
-        public String content { get; set; }
-        public List<GameComment> comments { get; set; }
-
-        public GamePosts(String id,
-                         String authorId,
-                         String gameId,
-                         String discussionId,
-                         int dislikesCount,
-                         int likesCount,
-                         String? description,
-                         String title,
-                         String content,
-                         List<GameComment> comments,
-                         DateTime createdAt)
-        {
-            this.id = id;
-            this.likesCount = likesCount;
-            this.comments = comments;
-            this.gameId = gameId;
-            this.authorId = authorId;
-            this.discussionId = discussionId;
-            this.dislikesCount = dislikesCount;
-            this.description = description;
-            this.title = title;
-            this.content = content;
-            this.createdAt = createdAt;
-        }
-=======
     public class GamePosts
     {
         public GamePosts()
@@ -66,6 +35,5 @@ namespace Slush.Data.Entity.Community.GameGroup
         public String? content { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
