3f7e2fa89ee4bb22908b41670829423fa86d7222
Administrator <admin@example.com>
2024-05-17 15:00:05 +0000

    Update 2 files
    - /Entity/Store/Product/Language/Language.cs
    - /Entity/Store/Product/Language/LanguageInGame.cs

--- Entity/Store/Product/Language/LanguageInGame.cs
+++ Entity/Store/Product/Language/LanguageInGame.cs
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-﻿using Slush.Entity.Abstract;
 
-namespace Slush.Data.Entity
-{
-    public class LanguageInGame : DBRecord
-    {
-        public String gameId { get; set; }
-        public String languageId { get; set; }
-
-        public LanguageInGame(String id,
-                              String gameId,
-                              String languageId,
-                              DateTime createdAt)
-=======
-﻿
 
 namespace Slush.Data.Entity
 {
@@ -24,15 +9,12 @@ namespace Slush.Data.Entity
         }
 
         public LanguageInGame(Guid id, Guid gameId, Guid languageId, DateTime? createdAt)
->>>>>>> development_branch
         {
             this.id = id;
             this.gameId = gameId;
             this.languageId = languageId;
             this.createdAt = createdAt;
         }
-<<<<<<< HEAD
-=======
 
 
 
@@ -41,6 +23,6 @@ namespace Slush.Data.Entity
         public Guid languageId { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
+
