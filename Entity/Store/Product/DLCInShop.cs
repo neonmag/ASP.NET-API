fac4c57bfc696d39e5024fed434f766f8961f70e
Administrator <admin@example.com>
2024-05-17 14:58:23 +0000

    Update 2 files
    - /Entity/Store/Product/DLCInShop.cs
    - /Entity/Store/Product/GameInShop.cs

--- Entity/Store/Product/DLCInShop.cs
+++ Entity/Store/Product/DLCInShop.cs
@@ -1,38 +1,4 @@
 ﻿using Slush.Data.Entity;
-<<<<<<< HEAD
-using Slush.Entity.Abstract;
-
-namespace Slush.Entity.Store.Product
-{
-    public class DLCInShop : Game
-    {
-        public String gameId;
-        public DLCInShop(String id,
-                         String name,
-                         float price,
-                         String image,
-                         DateTime dateOfRelease,
-                         String author,
-                         String publisher,
-                         List<String> categories,
-                         List<LanguageInGame> languages,
-                         String urlForContent,
-                         DateTime createdAt,
-                         String gameId)
-        {
-            this.id = id;
-            this.name = name;
-            this.price = price;
-            this.previeImage = image;
-            this.dateOfRelease = dateOfRelease;
-            this.developerId = author;
-            this.publisherId = publisher;
-            this.categoriesId = categories;
-            this.urlForContent = urlForContent;
-            this.createdAt = createdAt;
-            this.gameId = gameId;
-        }
-=======
 
 namespace Slush.Entity.Store.Product
 {
@@ -71,6 +37,5 @@ namespace Slush.Entity.Store.Product
 
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
--- Entity/Store/Product/GameInShop.cs
+++ Entity/Store/Product/GameInShop.cs
@@ -1,27 +1,4 @@
 ﻿using Slush.Data.Entity;
-<<<<<<< HEAD
-using Slush.Entity.Abstract;
-
-namespace Slush.Entity.Store.Product
-{
-    public class GameInShop : Game
-    {
-        public GameInShop(String id,
-                          String name,
-                          float price,
-                          int discount,
-                          String image,
-                          DateTime dateOfRelease,
-                          String author,
-                          String publisher,
-                          List<String> categories,
-                          List<String> gameImages,
-                          List<LanguageInGame> languages,
-                          String urlForContent,
-                          DateTime createdAt,
-                          List<String> languagesId,
-                          List<String> systemRequirementsId)
-=======
 
 
 namespace Slush.Entity.Store.Product
@@ -30,25 +7,11 @@ namespace Slush.Entity.Store.Product
     {
 
         public GameInShop(Guid id, String? name, float price, int discount, String? previeImage, DateTime dateOfRelease, Guid developerId, Guid publisherId, String? urlForContent, DateTime? createdAt)
->>>>>>> development_branch
         {
             this.id = id;
             this.name = name;
             this.price = price;
             this.discount = discount;
-<<<<<<< HEAD
-            this.gameImages = gameImages;
-            this.previeImage = image;
-            this.dateOfRelease = dateOfRelease;
-            this.languagesId = languagesId;
-            this.developerId = author;
-            this.publisherId = publisher;
-            this.categoriesId = categories;
-            this.systemRequirementsId = systemRequirementsId;
-            this.urlForContent = urlForContent;
-            this.createdAt = createdAt;
-        }
-=======
             this.previeImage = previeImage;
             this.dateOfRelease = dateOfRelease;
             this.developerId = developerId;
@@ -70,6 +33,5 @@ namespace Slush.Entity.Store.Product
         public String? urlForContent { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
