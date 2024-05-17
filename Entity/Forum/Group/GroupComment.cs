b0cd84a937db4988a017d6468a4258d028063f77
Administrator <admin@example.com>
2024-05-17 14:37:58 +0000

    Update GroupComment.cs

--- Entity/Forum/Group/GroupComment.cs
+++ Entity/Forum/Group/GroupComment.cs
@@ -1,27 +1,4 @@
-﻿using Slush.Data.Entity.Profile;
-<<<<<<< HEAD
-using Slush.Entity.Abstract;
-
-namespace Slush.Data.Entity.Community
-{
-    public class GroupComment : DBRecord
-    {
-        public String groupId { get; set; }
-        public String content { get; set; }
-
-
-        public GroupComment(String id,
-                            String content,
-                            DateTime createdAt,
-                            String userId,
-                            String groupId)
-        {
-            this.id = id;
-            this.content = content;
-            this.createdAt = createdAt;
-            this.groupId = groupId;
-        }
-=======
+using Slush.Data.Entity.Profile;
 
 
 namespace Slush.Data.Entity.Community
@@ -46,6 +23,5 @@ namespace Slush.Data.Entity.Community
         public Guid userId { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
