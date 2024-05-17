5d1b11ea84dd0fa2889058dec922d0ec216c43dc
Administrator <admin@example.com>
2024-05-17 14:29:23 +0000

    Update GameComment.cs

--- Entity/Forum/GameGroup/GameComment.cs
+++ Entity/Forum/GameGroup/GameComment.cs
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-﻿using Slush.Entity.Abstract;
-
-namespace Slush.Data.Entity.Community.GameGroup
-{
-    public class GameComment : DBRecord
-    {
-        public String gamePostId { get; set; }
-        public String content { get; set; }
-        public Author Author { get; set; }
-
-        public GameComment(String id,
-                           String gamePostId,
-                           String content,
-                           DateTime createdAt)
-=======
-﻿
-
 namespace Slush.Data.Entity.Community.GameGroup
 {
     public class GameComment
@@ -25,15 +7,10 @@ namespace Slush.Data.Entity.Community.GameGroup
         }
 
         public GameComment(Guid id, Guid gamePostId, String? content, Guid authorId, DateTime? createdAt)
->>>>>>> development_branch
         {
             this.id = id;
             this.gamePostId = gamePostId;
             this.content = content;
-<<<<<<< HEAD
-            this.createdAt = createdAt;
-        }
-=======
             this.authorId = authorId;
             this.createdAt = createdAt;
         }
@@ -43,6 +20,5 @@ namespace Slush.Data.Entity.Community.GameGroup
         public Guid authorId { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
