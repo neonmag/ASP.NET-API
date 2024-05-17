dc5975b82722e75a6fb6fe0de0a81c89bef61fc9
Administrator <admin@example.com>
2024-05-17 14:36:59 +0000

    Update Group.cs

--- Entity/Forum/Group/Group.cs
+++ Entity/Forum/Group/Group.cs
@@ -1,35 +1,4 @@
-﻿using Slush.Data.Entity.Profile;
-<<<<<<< HEAD
-using Slush.Entity.Abstract;
-
-namespace Slush.Data.Entity.Community
-{
-    public class Group : Category
-    {
-        public List<User> users { get; set; }
-        public List<Topic> topics { get; set; }
-        public List<GroupComment> comments { get; set; }
-
-
-        public Group(String id,
-                     String name,
-                     String description,
-                     List<User> users,
-                     List<Topic> topics,
-                     List<GroupComment> comments,
-                     DateTime createdAt,
-                     DateTime? deleteAt)
-        {
-            this.id = id;
-            this.name = name;
-            this.description = description;
-            this.users = users;
-            this.topics = topics;
-            this.comments = comments;
-            this.createdAt = createdAt;
-            this.deleteAt = deleteAt;
-        }
-=======
+using Slush.Data.Entity.Profile;
 
 
 namespace Slush.Data.Entity.Community
@@ -55,6 +24,5 @@ namespace Slush.Data.Entity.Community
         public String? description { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
