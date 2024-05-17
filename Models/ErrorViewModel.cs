9d834e1464b6a2ccc6e17a17034352d4ddb4c0f7
Administrator <admin@example.com>
2024-05-17 15:03:44 +0000

    Update file ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,14 +2,8 @@ namespace Slush.Models
 {
     public class ErrorViewModel
     {
-<<<<<<< HEAD
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-=======
         public String? RequestId { get; set; }
 
         public bool ShowRequestId => !String.IsNullOrEmpty(RequestId);
->>>>>>> development_branch
     }
 }
