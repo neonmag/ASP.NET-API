c2584e6ceaef02376d629e557901b8bc50770c7e
Administrator <admin@example.com>
2024-05-17 14:44:05 +0000

    Update OwnedGame.cs

--- Entity/Profile/OwnedGame.cs
+++ Entity/Profile/OwnedGame.cs
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-﻿using Slush.Entity.Abstract;
 
-namespace Slush.Entity.Profile
-{
-    public class OwnedGame : ProfileGame
-    {
-        public OwnedGame(string id,
-                         string ownedGameId,
-                         string userId,
-                         DateTime createdAt)
-=======
-﻿
 
 namespace Slush.Entity.Profile
 {
@@ -21,20 +9,16 @@ namespace Slush.Entity.Profile
         }
 
         public OwnedGame(Guid id, Guid ownedGameId, Guid userId, DateTime? createdAt)
->>>>>>> development_branch
         {
             this.id = id;
             this.ownedGameId = ownedGameId;
             this.userId = userId;
             this.createdAt = createdAt;
         }
-<<<<<<< HEAD
-=======
         public Guid id { get; set; }
         public Guid ownedGameId { get; set; }
         public Guid userId { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
