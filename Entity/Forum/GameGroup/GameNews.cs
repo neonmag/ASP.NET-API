dbc858861325b68e5145fe3429bde1756fabea67
Administrator <admin@example.com>
2024-05-17 14:32:51 +0000

    Update GameNews.cs

--- Entity/Forum/GameGroup/GameNews.cs
+++ Entity/Forum/GameGroup/GameNews.cs
@@ -1,34 +1,5 @@
-﻿namespace Slush.Data.Entity.Community.GameGroup
+namespace Slush.Data.Entity.Community.GameGroup
 {
-<<<<<<< HEAD
-    public class GameNews : Slush.Entity.Abstract.Post
-    {
-        public String content { get; set; }
-
-        public GameNews(String id,
-                         String authorId,
-                         String gameId,
-                         String discussionId,
-                         int dislikesCount,
-                         int likesCount,
-                         String? description,
-                         String gameGroupId,
-                         String title,
-                         String content,
-                         DateTime createdAt)
-        {
-            this.id = id;
-            this.likesCount = likesCount;
-            this.gameId = gameId;
-            this.authorId = authorId;
-            this.discussionId = discussionId;
-            this.dislikesCount = dislikesCount;
-            this.description = description;
-            this.title = title;
-            this.content = content;
-            this.createdAt = createdAt;
-        }
-=======
     public class GameNews
     {
         public GameNews()
@@ -61,6 +32,5 @@
         public String? content { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
