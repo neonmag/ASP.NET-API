c67625eac6826bbe6e0c1189750985c54126b36b
Administrator <admin@example.com>
2024-05-17 14:46:32 +0000

    Update User.cs

--- Entity/Profile/User.cs
+++ Entity/Profile/User.cs
@@ -1,45 +1,10 @@
-﻿using Slush.Data.Entity.Community;
-<<<<<<< HEAD
-using Slush.Entity.Abstract;
-=======
+using Slush.Data.Entity.Community;
 
->>>>>>> development_branch
 using Slush.Entity.Profile;
 using Slush.Entity.Store.Product;
 
 namespace Slush.Data.Entity.Profile
 {
-<<<<<<< HEAD
-    public class User : DBRecord
-    {
-        public String name { get;set; }
-        public String passwordSalt { get;set; }
-        public String salt { get;set; }
-        public String email { get;set; }
-        public String phone { get;set; }
-        public List<OwnedGame> ownedGames { get; set; }
-        public List<WishedGame> wishedGames { get; set; }
-        public List<Friends> friends { get; set; }
-        public List<UserComment> comments { get; set; }
-        public List<Screenshot> screenshots { get; set; }
-        public List<Video> videos { get; set; }
-        public List<Group> groups { get; set; }
-
-
-        public User(String id,
-                    String name,
-                    String passwordSalt,
-                    String email,
-                    String phone,
-                    List<OwnedGame> ownedGames,
-                    List<WishedGame> wishedGames,
-                    List<Friends> friends,
-                    DateTime createdAt,
-                    String salt,
-                    List<Screenshot> screenshots,
-                    List<Video> videos,
-                    List<Group> groups)
-=======
     public class User 
     {
         public User()
@@ -47,24 +12,10 @@ namespace Slush.Data.Entity.Profile
         }
 
         public User(Guid id, String? name, String? passwordSalt, String? salt, String? email, String? phone, DateTime? createdAt)
->>>>>>> development_branch
         {
             this.id = id;
             this.name = name;
             this.passwordSalt = passwordSalt;
-<<<<<<< HEAD
-            this.email = email;
-            this.phone = phone;
-            this.ownedGames = ownedGames;
-            this.wishedGames = wishedGames;
-            this.friends = friends;
-            this.createdAt = createdAt;
-            this.salt = salt;
-            this.screenshots = screenshots;
-            this.videos = videos;
-            this.groups = groups;
-        }
-=======
             this.salt = salt;
             this.email = email;
             this.phone = phone;
@@ -82,6 +33,5 @@ namespace Slush.Data.Entity.Profile
 
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
