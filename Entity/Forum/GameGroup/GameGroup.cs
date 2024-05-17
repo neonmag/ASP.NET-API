124a2dd3cc83c0d437407599742990909006c95f
Administrator <admin@example.com>
2024-05-17 14:30:35 +0000

    Update GameGroup.cs

--- Entity/Forum/GameGroup/GameGroup.cs
+++ Entity/Forum/GameGroup/GameGroup.cs
@@ -1,40 +1,9 @@
-﻿using Slush.Data.Entity.Profile;
-<<<<<<< HEAD
-using Slush.Entity.Abstract;
-=======
+using Slush.Data.Entity.Profile;
 
->>>>>>> development_branch
 using Slush.Entity.Store.Product;
 
 namespace Slush.Data.Entity.Community.GameGroup
 {
-<<<<<<< HEAD
-    public class GameGroup : DBRecord
-    {
-        public GameInShop game { get; set; }
-        public List<GameNews> news { get; set; }
-        public List<Video> videos { get; set; }
-        public List<Screenshot> screenshots { get; set; }
-        public List<GameTopic> topics { get; set; }
-
-        public GameGroup(String id,
-                         GameInShop game,
-                         List<GameNews> news,
-                         List<GameVideo> videos,
-                         List<GameScreenshot> screenshots,
-                         List<GameTopic> topics,
-                         DateTime createdAt)
-        {
-            this.id = id;
-            this.game = game;
-            this.news = news;
-            this.videos = videos;
-            this.screenshots = screenshots;
-            this.topics = topics;
-            this.createdAt = createdAt;
-        }
-
-=======
     public class GameGroup
     {
         public GameGroup()
@@ -52,6 +21,5 @@ namespace Slush.Data.Entity.Community.GameGroup
         public Guid gameId { get; set; }
         public DateTime? createdAt { get; set; }
         public DateTime? deleteAt { get; set; }
->>>>>>> development_branch
     }
 }
